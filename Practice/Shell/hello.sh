e441f07a7df1f47385baa75e0c03b5dc24969b06
Varni-tech <vjbhuva23@gmail.com>
2026-02-20 17:15:35 +0530

    Shell Script Demo

---
+++ Practice/Shell/hello.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This Script for Practice
+
+echo "Varni : Hello, Everyone!"
+
+echo "Team : Hello, Varni"
+
+echo "Varni : How are you?"
+
+echo "Team : We are Fine, What about You?"
