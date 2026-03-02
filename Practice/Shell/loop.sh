599d5674159692f9eeae3575c6091541bca76729
Varni-tech <vjbhuva23@gmail.com>
2026-03-02 11:25:43 +0530

    updated loop

--- Practice/Shell/loop.sh
+++ Practice/Shell/loop.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
 
 << loops
-This is for for loop and While loop Learning Purpose
+# This is for for loop and While loop Learning Purpose
 loops
 
-<< Practice 
+<< for_loop_Practice 
+
 for (( num=1 ; num <=10 ; num++ ));
 do
 	mkdir "demo$num"
 done
-Practice
 
+for_loop_Practice
+
+<< for_loop_with_arguments
 # $1 is argument 1 which is folder name 
 # $2 is argument 2 which is Start range 
 # $3 is argument 3 which is end range 
-
 
 for (( num=$2 ; num<=$3 ; num++ ))
 do
 	mkdir "$1$num"
 done	
 
+for_loop_with_arguments
+
+<< while_loop_practice
+while_loop_practice
+num=o
+while [[ $num%2==0 && $num -le 10 ]]
+do 
+	echo $num 
+	num=$((num+1))  
+done 
